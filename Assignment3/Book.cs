de4ff3bc79d6f5a9d599c9c85d93da655bbeb5bd
Rajesh Sampath <Rajesh Sampath@RAJESH-PC>
2016-11-18 23:32:50 -0500

    Add project files.

---
+++ Assignment3/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Book
+    {
+        private int catalogNumber;
+        private string title;
+        private string authors;
+        Customer c;
+        bool available;
+
+        public int CatalogNumber
+        {
+            get { return catalogNumber; }
+        }
+
+        public Book(string title, string authors, int catalogNo)
+        {
+            this.title = title;
+            this.authors = authors;
+            this.catalogNumber = catalogNo;
+        }
+
+        public string ToString() 
+        {
+            string s =String.Format( "{0,-5}{1,-20}{2,-10}",catalogNumber,title ,authors);
+            if(this.c == null)
+            {
+                s = s + "\tAvailable";
+            }
+            else if (available == true)
+            {
+                s = s + "\tAvailable";
+            }
+            else if (available == false)
+            {
+                s = s + "\tChecked out to Customer" +" "+ c.Id;
+            }
+            return s;
+        }
+
+        public bool CheckOut(Customer c)
+        {            
+            if (this.c == null)
+            {
+                this.c = c;
+                available = true;
+            }
+            else
+            {
+                available = false;
+            }
+            return available;
+        }
+
+        public bool CheckIn()
+        {
+            if(this.c!=null)
+            {
+                this.c = null;
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+    }
+}
---
+++ Assignment3/Library.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Library
+    {
+        private Customer[] customerArray = new Customer[5];
+        private Book[] bookArray = new Book[5];
+        private int custId = 0;
+        private int catalogNum = 100;
+
+        public bool AddNewCustomer(string customerName)
+        {
+            custId = custId + 1;
+            for (int i = 0; i < customerArray.Length; i++)
+            {
+                
+                if (customerArray[i] == null)
+                {
+                    Customer c1 = new Customer(customerName, custId);
+                    customerArray[i] = c1;
+                    return true;
+
+                }
+                else continue;
+            }
+            return false;
+        }
+
+        public bool AddNewBook(string bookTitle, string bookAuthor)
+        {
+            catalogNum = catalogNum + 1;
+            for (int x = 0; x < bookArray.Length; x++)
+            {
+                if (bookArray[x] == null)
+                {
+                    Book b1 = new Book(bookTitle, bookAuthor, catalogNum);
+                    bookArray[x] = b1;
+                    return true;
+                }
+                else continue;
+            }
+            return false;
+        }
+
+        public string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < customerArray.Length; i++)
+
+            {
+                if (customerArray[i] != null)
+                {
+                    s = s+customerArray[i].ToString()+"\n" ;
+                }
+            }
+            s = s + "\n";
+
+            for (int m = 0; m<bookArray.Length; m++)
+            {
+                if (bookArray[m] != null)
+                {
+                    s = s + bookArray[m].ToString()+"\n";
+                }
+            }
+            return s;     
+        }
+    }
+
+    
+}
